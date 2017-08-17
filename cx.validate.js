f973b0fd22ed55269bfa474cc1123a1044c725f4
cenxi <fengxijdjug001@163.com>
2017-08-17 10:44:59 +0800

    regex plugin

---
+++ cx.validate.js
@@ -0,0 +1,78 @@
+(function(){
+                $.regex = {
+                    rules: {
+                        email: {
+                            validator: function (_4eb) {
+                                return /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i.test(_4eb);
+                            },
+                            message: "Please enter a valid email address."
+                        },
+                        url: {
+                            validator: function (_4ec) {
+                                return /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(_4ec);
+                            },
+                            message: "Please enter a valid URL."
+                        },
+                        notempty: {
+                            validator: function (str) {
+                                return !(/^\s*$/.test(str));
+                            },
+                            message: "非空，请填入对应参数。"
+                        },
+                        ip: {
+                            validator: function (str) {
+                                return /^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9]{1,2})(\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9]{1,2})){3}$/.test(str);
+                            },
+                            message: "请填入正确的ip地址。"
+                        },
+                        intRange: {
+                            validator: function (str, range) {
+                                return (/^\d+$/.test(str))&&str>=range[0]&&str<=range[1];
+                            },
+                            message: "请填入{0} - {1}范围内的值。"
+                        }
+                    },
+                };
+                $.extend({
+                    validateExe:function () {
+                        $.message = "";
+                        $.errorInput={};
+                        $('input[validType]').each(function () {
+                            var input=this;
+                            var val=$(this).val();
+                            var validType = $(this).attr('validType');
+                            $.each(validType.split(';'), function () {
+                                var type=this.split('[')[0];
+                                if(!!this.split('[')[1]) {
+                                    var params=this.split('[')[1].substr(0,this.split('[')[1].length-1);
+                                    var param=[];
+                                    $.each(params.split(','),function () {
+                                        param.push(parseInt(this));
+                                    });
+                                    console.log(param)
+                                    if(!$.regex.rules[type].validator(val,param)){
+                                        $.message=$.regex.rules[type].message;
+                                        var i=0;
+                                        $.each(param,function(){
+                                            $.message=$.message.replace("{" + i + "}", this);
+                                            i++;
+                                        })
+                                        input.focus();
+                                        $.errorInput=input;
+                                        return false;
+                                    }
+                                }else{
+                                    if(!$.regex.rules[type].validator(val)){
+                                        $.message=$.regex.rules[type].message;
+                                        input.focus();
+                                        $.errorInput=input;
+                                        return false;
+                                    }
+                                }
+                            });
+                        });
+                    },
+                    message:"",
+                    errorInput:{}
+                })
+            })();
